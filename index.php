2bafc7dcf532bdcf27872cd94c8e5b99cc206007
[.::MDT::.] <mdt86@yahoo.it>
2013-08-20 12:14:17 +0200

    Update index.php
    Added CSS style at the beginning.
    Added automatic folder/host detection via PHP variables.
    Fixed ADD EXTERNAL KML feature.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
   <!-- 
-  original GeoPHP https://code.google.com/p/geophp/ released under Apache Licence 2.0
+  original GeoPHP https://code.google.com/p/geophp/ released under Apache Licence 2.0,
   modified by Michele Dalla Torre
   -->
 <!DOCTYPE html>
@@ -7,20 +7,70 @@
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
-      #map-canvas { height: 100% }
+      
+	  html { 
+		height: 100%
+		}
+      
+	  body { 
+		height: 100%; 
+		margin: 5; 
+		padding: 0;
+		background-color: light-grey
+		}
+		
+		h1 {
+			font-family:Georgia, "Arial", Serif
+		}
+      
+	  #map-canvas { 
+		height: 100% 
+		}
+	  
+	  #status { 
+		float:bottom; 
+		clear:left; 
+		overflow-vertical:scroll; 
+		height: 200px; 
+		width:1200px; 
+		border:0px dotted black;
+
+		}
+	
+	#sidebar {
+		float:left; 
+		overflow-vertical:scroll; 
+		height: 600px; 
+		width:300px; 
+		border:1px dashed black; 
+		background-color:#D3D3D3
+	}
+	
+	#map_canvas {
+		width: 900px; 
+		height: 600px; 
+		float:left; 
+		border: 0px solid black
+	}
+		
     </style>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false">
-    </script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     <script type="text/javascript">
 		var userAdded = 1;
 		var layers=[];
-		var basedir = "YOUR_WEB_ADDRESS"; //e.g. www.mywebsite.it/mymaps/
 		
 		<?php
 		$dir = ".";
-		$basedir = "YOUR_WEB_ADDRESS"; //e.g. www.mywebsite.it/mymaps/
+		
+		//////////////// CONFIG VARIABLES /////////////
+		// e.g. http://www.mywebsite.com				  
+		$mywebsite = "http://" . $_SERVER['HTTP_HOST'];	  
+		// e.g. /mymaps													  
+		$currentfolder = dirname($_SERVER['PHP_SELF']); 
+		// e.g. http://www.mywebsite.com/mymaps/	
+		$basedir = $mywebsite . $currentfolder . "/";     
+		///////////////////////////////////////////////
+		
 		$i=0;
 		$dir = @opendir($dir) or die("Ocorreu Um Erro Ao Tentar Abrir a dir: $dir");
 		while ($file = readdir($dir)) {
@@ -43,6 +93,8 @@
 		closedir($dir);
 		$i=0;
 		?>
+		
+		var numlayers = layers.length;
 
 		var map;
 	
@@ -56,6 +108,8 @@
 				  }
 				  
 				  map = new google.maps.Map(document.getElementById('map_canvas'),myOptions);
+				  			  
+				  document.getElementById("url").value = "http://";
 				  
 				  // create HTML table with all KML files
 				  for(var layer in layers) {
@@ -81,18 +135,16 @@
 		  theUrl = theUrl.replace(/^\s+/, "");
 		  theUrl = theUrl.replace(/\s+$/, "");
 		  theUrl = URLEncode(theUrl);
-		  alert('error');
-		  if (theUrl.indexOf(' ') != -1) {
+		  
+		  if (theUrl.indexOf(' ') != -1 ) {
 			alert('Error - that address has a space in it');
 		  } else {
 			var id = "userAdded" + userAdded;
-			layers[id] = {};
+			layers[id] = new google.maps.KmlLayer(theUrl, {preserveViewport: true});
 			layers[id].url = theUrl;
-			layers[id].name = "User Layer " + userAdded;
-
 			addTR(id);
 			document.getElementById(id).checked = true;
-			toggleGeoXML(id, true);
+			toggleLayers(id);			
 			userAdded++;
 		  }
 		}
@@ -134,14 +186,14 @@
 			return encoded;
 		};
 
-		// create a new HTML TR element foreach KML file (i.e. map layer)
+		// create a new HTML TR element for each KML file (i.e. map layer)
 		function addTR(id) {
 		  var layerTR = document.createElement("tr");
 		  var inputTD = document.createElement("td");
 
 		  var input = document.createElement("input");
 		  input.type = "checkbox";
-		  input.id = "mylayer" + id;
+		  input.id = id;
 		  input.onclick = function ()  { toggleLayers(id) };
 		  inputTD.appendChild(input);
 
@@ -152,7 +204,7 @@
 		  // the full URL with basedir + filename + extension (.kml)
 		  var myfullurl = layers[id].url;
 		  // get intex of last / chart
-		  var myindex = myfullurl.lastIndexOf("/")+1
+		  var myindex = myfullurl.lastIndexOf("/") + 1;
 		  var name = document.createTextNode(myfullurl.substring(myindex));
 		  nameA.appendChild(name);
 		  nameTD.appendChild(nameA);
@@ -161,26 +213,29 @@
 		  layerTR.appendChild(nameTD);
 		  document.getElementById("sidebarTBODY").appendChild(layerTR);
 		}
+		
+		
+		
 	</script>
   </head>
   <body onload="initialize()">
-  
-    <br/>
+	
+	<h1> Google Maps KML Viewer </h1>
+
     <input id="url" value="" size="60"/>
-    <input type="button" value="Add" onClick="addGeoXML();"/>
+    <input type="button" value="Add new KML file!" onClick="addGeoXML();"/>
     <br/>
     <br/>
 		
-	<div id="map_canvas" style="width: 800px; height: 600px; float:left; border: 1px solid black;">
+	<div id="map_canvas">
 	</div>
 	
-	<div id="sidebar" style="float:left; overflow-vertical:scroll; height: 600px; width:300px; border:1px solid black">
-		<form name="geo">
-			<table id="sidebarTABLE">
+	<div id="sidebar">
+		<table id="sidebarTABLE">
+			<b>KML layers</b>
 			<tbody id="sidebarTBODY">
 			</tbody>
-			</table>
-		</form>
+		</table>
     </div>
 	
 	<div id="status">
@@ -188,5 +243,3 @@
 	
   </body>
 </html>
-
-
